58418d5f2f13198d6106f057b9bbc02a3e6753b4
Kishan Jha <souljha797@gmail.com>
2026-09-22 15:46:36 +0530

    Time: 22 ms (12.23%), Space: 69.6 MB (30.87%) - LeetHub

--- 2956-find-common-elements-between-two-arrays/2956-find-common-elements-between-two-arrays.cpp
+++ 2956-find-common-elements-between-two-arrays/2956-find-common-elements-between-two-arrays.cpp
@@ -6,11 +6,11 @@ public:
 
         int com1 = 0, com2 = 0;
 
-        // Count indices in nums1 that exist in nums2
+       
         com1 = count_if(nums1.begin(), nums1.end(),
                         [&](int x){ return s2.count(x); });
 
-        // Count indices in nums2 that exist in nums1
+        
         com2 = count_if(nums2.begin(), nums2.end(),
                         [&](int x){ return s1.count(x); });
 
