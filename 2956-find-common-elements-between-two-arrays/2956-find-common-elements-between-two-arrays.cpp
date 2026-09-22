af0022c5d95011bd78c411a2dc10054c84eb613c
Kishan Jha <souljha797@gmail.com>
2026-09-22 15:46:20 +0530

    Time: 15 ms (30.86%), Space: 69.4 MB (36.68%) - LeetHub

--- 2956-find-common-elements-between-two-arrays/2956-find-common-elements-between-two-arrays.cpp
+++ 2956-find-common-elements-between-two-arrays/2956-find-common-elements-between-two-arrays.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
     vector<int> findIntersectionValues(vector<int>& nums1, vector<int>& nums2) {
-        // Convert nums2 into a set for O(1) lookup
-        unordered_set<int> s2(nums2.begin(), nums2.end());
         unordered_set<int> s1(nums1.begin(), nums1.end());
+        unordered_set<int> s2(nums2.begin(), nums2.end());
 
         int com1 = 0, com2 = 0;
 
-        // Directly count without extra loops on sets
-        for (int x : nums1) {
-            if (s2.count(x)) com1++;
-        }
-        for (int x : nums2) {
-            if (s1.count(x)) com2++;
-        }
+        // Count indices in nums1 that exist in nums2
+        com1 = count_if(nums1.begin(), nums1.end(),
+                        [&](int x){ return s2.count(x); });
+
+        // Count indices in nums2 that exist in nums1
+        com2 = count_if(nums2.begin(), nums2.end(),
+                        [&](int x){ return s1.count(x); });
 
         return {com1, com2};
     }
